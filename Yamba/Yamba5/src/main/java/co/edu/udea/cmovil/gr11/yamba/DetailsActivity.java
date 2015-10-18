243b66bd5ce9e9476820c1d29fc868e2b1f65d89
Felipe Marin <lfelipe.marin@gmail.com>
2015-10-17 19:39:27 -0500

    Yamba funcionando con listas. Fin del lab 6

---
+++ Yamba/Yamba5/src/main/java/co/edu/udea/cmovil/gr11/yamba/DetailsActivity.java
@@ -0,0 +1,47 @@
+package co.edu.udea.cmovil.gr11.yamba;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Check if this activity was created before
+        if (savedInstanceState == null) {
+
+            // Create a fragment
+            DetailsFragment fragment = new DetailsFragment(); //
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment,
+                            fragment.getClass().getSimpleName()).commit(); //
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- Yamba/Yamba5/src/main/java/co/edu/udea/cmovil/gr11/yamba/MainActivity.java
+++ Yamba/Yamba5/src/main/java/co/edu/udea/cmovil/gr11/yamba/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -27,7 +28,7 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_settings:
                 startActivity(new Intent(this, SettingsActivity.class));
                 return true;
@@ -36,6 +37,12 @@ public class MainActivity extends Activity {
                 return true;
             case R.id.action_refresh:
                 startService(new Intent(this, RefreshService.class));
+                return true;
+            case R.id.action_purge:
+                int rows = getContentResolver().delete(
+                        StatusContract.CONTENT_URI, null, null);
+                Toast.makeText(this, "Deleted " + rows + " rows",
+                        Toast.LENGTH_LONG).show();
                 return true;
             default:
                 return false;
